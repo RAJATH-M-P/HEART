0ac2c77680c2317c1b2492aefa2412db7d1a6705
Rajath M P <rajathmp955@gmail.com>
2026-09-19 18:35:57 +0530

    Complete full-stack infrastructure: Fix React entry files and finalize project structure

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReportUpload from './components/ReportUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <ReportUpload />
+    </div>
+  );
+}
+
+export default App;
